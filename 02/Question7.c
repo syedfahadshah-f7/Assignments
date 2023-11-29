5b3e1d19da63275d1cb0b95457c42d679fb8e2c5
syedfahadshah-f7 <142867460+syedfahadshah-f7@users.noreply.github.com>
2023-11-29 15:37:43 +0500

    Update Question7.c

--- 02/Question7.c
+++ 02/Question7.c
@@ -1,42 +1,45 @@
 
 #include <stdio.h>
-void sorting(int ages[],int prices[], int n){
-    ages[n];
-    prices[n];
-    
- 
+void sorting(int arr[][],int n){
+    arr[2][n];
     for(int i=0;i<n;i++){
         int temp=0;
         for(int j=0;j<n;j++){
-            if(ages[i]<ages[j]){
-                temp = ages[i];
-                ages[i]=ages[j];
-                ages[j]=temp;
+            if(arr[1][j]>arr[1][j+1]){
+                temp = arr[1][j];
+                arr[1][j]=arr[1][j+1];
+                ages[1][j+1]=temp;
             }
-            else if(prices[i]>prices[j]){
-                temp = prices[i];
-                prices[i]=prices[j];
-                prices[j]=temp;
+             if(arr[2][j]<arr[2][j+1]{
+                temp = arr[2][j];
+                arr[2][j]=arr[2][j+1];
+                arr[2][j+1]=temp;
             }
         }// end j loop
     }// end i loop
-      printf("Sorted list of ages in assending order:");
-   for(int i=0;i<n;i++){
-       printf("%d,", ages[i]);
+   for(int i=0;i<2;i++){
+   for(int j=0;j<n;j++){
+       printf("%d,", arr[i][j]);
    }
-   printf("\n");
-   printf("Sorted list of prices in dessending order:");
-   for(int i=0;i<n;i++){
-       printf("%d,", prices[i]);
+    printf("\n");
+       
    }
 }// end function
 
 int main() {
     int n=5;
-    int ages[]={15,20,45,35,40};
-    int prices[]={800,700,400,500,900};
+    int arr[2][5]={{15,20,45,35,40},
+                {800,700,400,500,900}};
+                for(int i=0;i<2;i++){
+    printf("Initial Array");
+   for(int j=0;j<n;j++){
+       printf("%d,", arr[i][j]);
+   }
+    printf("\n");
+       
+   }
     
-    sorting(ages,prices,n);
+    sorting(arr,n);
   
     return 0;
 }
